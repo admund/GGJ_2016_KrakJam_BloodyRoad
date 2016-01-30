31bdf6d635b36b3840204b27dd1cd18cbc5e80c4
Adrian Gardian <gardian.adrian@gmail.com>
2016-01-30 03:52:24 +0100

    pentagram anim

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -4,8 +4,12 @@ extends Area
 var buff_area_types = preload("buff_area_type.gd").new()
 var player_node = null
 
+var timeout = 0
+var fade_out_start = false
+
 func _ready():
-	player_node = get_node("../Player")
+	set_fixed_process(true)
+	player_node = get_node("../../Player")
 	pass
 
 func _on_buff_area_body_exit( body ):
@@ -18,3 +22,13 @@ func _on_buff_area_body_enter( body ):
 	print("hit")
 	player_node.set_is_in_buff_area(buff_area_types.HEAL)
 	pass # replace with function body
+	
+	
+func _fixed_process(delta):
+	timeout += delta
+	if (timeout > 2.5 && !fade_out_start):
+		get_node("Sprite3D/AnimationPlayer").play("fade_out")
+		fade_out_start = true
+		
+	if (timeout > 4):
+		queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,11 +9,17 @@ var state_down  = input_states.new("down")
 var state_left  = input_states.new("left")
 var state_right = input_states.new("right")
 var state_x     = input_states.new("x")
+var state_1     = input_states.new("1")
+var state_2     = input_states.new("2")
+var state_3     = input_states.new("3")
 
 ################################3
 var bullet_prototype = preload("res://scenes/bullet.scn")
-var buff_area_types = preload("buff_area_type.gd").new()
 var bullets_node = null
+
+var buff_area_prototype = preload("res://scenes/arena.scn")
+var buff_area_types = preload("buff_area_type.gd").new()
+var buff_areas_node = null
 
 var btn_up      = null
 var btn_down    = null
@@ -21,6 +27,9 @@ var btn_left    = null
 var btn_right   = null
 
 var btn_x       = null
+var btn_1       = null
+var btn_2       = null
+var btn_3       = null
 
 var is_in_buff_area = buff_area_types.NONE
 
@@ -42,6 +51,9 @@ func process_input():
 	btn_left  = state_left.check()
 	btn_right = state_right.check()
 	btn_x =     state_x.check()
+	btn_1 =     state_1.check()
+	btn_2 =     state_2.check()
+	btn_3 =     state_3.check()
 	
 	move_vector = Vector3( 0, 0, 0 )
 	if( btn_up   > 1 ):
@@ -60,13 +72,21 @@ func process_input():
 		
 	if ( btn_x == 1 ):
 		create_bullet()
-		pass
-			
+		
+	if (btn_1 == 1):
+		create_buff_area(1)
+	
+	if (btn_2 == 1):
+		create_buff_area(2)
+	
+	if (btn_3 == 1):
+		create_buff_area(3)
 	
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
 	bullets_node = get_node("../bullets")
+	buff_areas_node = get_node("../buff_areas")
 	pass
 
 func set_is_in_buff_area(buff_area_type):
@@ -74,7 +94,6 @@ func set_is_in_buff_area(buff_area_type):
 	pass
 	
 func create_bullet():
-#	print("create_bullet")
 	var bullet = bullet_prototype.instance()
 	bullet.set_translation(self.get_translation() + Vector3(0, 10, 0))
 	var bullet_velocity_x = 0
@@ -86,3 +105,11 @@ func create_bullet():
 	bullet.velocity = Vector3(bullet_velocity_x,0,0)
 	bullets_node.add_child(bullet)
 	pass
+	
+func create_buff_area(buff_area_type):
+	var buff_area = buff_area_prototype.instance()
+	buff_area.set_translation(self.get_translation())
+	buff_areas_node.add_child(buff_area)
+	pass
+	
+	
